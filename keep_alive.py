4d6852a03e53cba8bc9dbb97a3aca5ca657c6397
Catalin Dumitru <62836637+catalindumitru@users.noreply.github.com>
2025-03-30 18:41:29 +0100

    Add keep_alive script

---
+++ keep_alive.py
@@ -0,0 +1,17 @@
+import requests
+import schedule
+import time
+
+# Replace with your Render.com server URL
+SERVER_URL = "https://score-mate.onrender.com/healthz"
+
+
+def keep_server_alive():
+    try:
+        response = requests.get(SERVER_URL)
+        print(f"Pinged server: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to reach server: {e}")
+
+
+keep_server_alive()
